4b9452fcc9c62d41d39760b1ce2a90ad47d922df
Samuel Stuart <sasdallas765@gmail.com>
2025-07-22 09:40:04 -0500

    [BUILD] Configure image subsystem and base

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -3,3 +3,11 @@
 function(set_image_entrypoint MODULE ENTRY)
     target_link_options(${MODULE} PRIVATE "-Wl,--entry,${ENTRY}")
 endfunction()
+
+function(set_image_base MODULE BASE)   
+    target_link_options(${MODULE} PRIVATE "-Wl,--image-base,${BASE}")
+endfunction()
+
+function(set_image_subsystem MODULE SUBSYSTEM)
+    target_link_options(${MODULE} PRIVATE "-Wl,--subsystem,${SUBSYSTEM}:5.01")
+endfunction()
